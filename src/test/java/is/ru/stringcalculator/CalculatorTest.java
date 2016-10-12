cbfd90e647463864b227fb8290bca38c95cb3ab0
tinnathuridur <tinnath@Tinnas-MBP.lan>
2016-10-12 17:57:27 +0000

    a test for the empty string

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -6,27 +6,11 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	public static void main(String args[]) {
-      org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
+        org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
     }
 
-	@Test
+    @Test
 	public void testEmptyString() {
-		assertEquals(0, Calculator.add(""));
-	}
-
-	@Test
-	public void testOneNumber() {
-		assertEquals(1, Calculator.add("1"));
-	}
-
-	@Test
-	public void testTwoNumbers() {
-		assertEquals(3, Calculator.add("1,2"));
-	}	
-
-	@Test
-    public void testMultipleNumbers(){
-    	assertEquals(6, Calculator.add("1,2,3"));
-    }
-
+	    assertEquals(0, Calculator.add(""));
+	} 
 }
